00654ccf8a17ad158b06713f4cfcbffa960a0d70
Adam Harvey <heyitsmeharv@gmail.com>
2019-09-22 11:29:49 +0100

    conditional title added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useRef, useEffect } from "react";
-import styled from 'styled-components'
+import styled from 'styled-components';
 
-import AddTaskInput from './components/AddTaskInput';
+import { Add } from 'styled-icons/material/Add';
 
 import colour from './resources/styles/colours';
 
@@ -17,10 +17,129 @@ const AppContainer = styled.div`
   background: linear-gradient(90deg, rgba(255,136,0,1) 0%, rgba(255,196,0,1) 68%, rgba(255,222,0,1) 100%, rgba(255,141,0,1) 100%);
 `
 
+const TodoListContainer = styled.div`
+  width: 50%;
+  height: 50%;
+  border: solid 4px ${colour.background}
+  background: ${colour.white}
+`
+
+const TaskTitle = styled.h1`
+  font-size: 2.5rem;
+  display: flex;
+  justify-content: center;
+  color: ${colour.black};
+`
+
+const InputContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+const Form = styled.form`
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+  background-color: ${colour.complement};
+  width: ${props => (props.barOpened ? "30rem" : "2rem")};
+  cursor: ${props => (props.barOpened ? "auto" : "pointer")};
+  padding: 2rem;
+  height: 2rem;
+  border-radius: 10rem;
+  transition: all 300ms cubic-bezier(0.645, 0.045, 0.355, 1);
+`;
+
+const Input = styled.input`
+  font-size: 14px;
+  line-height: 1;
+  background-color: transparent;
+  width: 100%;
+  margin-left: ${props => (props.barOpened ? "1rem" : "0rem")};
+  border: none;
+  color: white;
+  transition: all 300ms cubic-bezier(0.645, 0.045, 0.355, 1);
+
+  &:focus,
+  &:active {
+    outline: none;
+  }
+  &::placeholder {
+    color: white;
+  }
+`;
+
+const Button = styled.button`
+  line-height: 1;
+  pointer-events: ${props => (props.barOpened ? "auto" : "none")};
+  cursor: ${props => (props.barOpened ? "pointer" : "none")};
+  background-color: transparent;
+  border: none;
+  outline: none;
+  color: white;
+`;
+
+const AddIcon = styled(Add)`
+  width: 2rem;
+  height: 2rem;
+`
+
 function App() {
+  const [tasks, setTasks] = useState(0);
+  const [input, setInput] = useState("");
+  const [barOpened, setBarOpened] = useState(false);
+  const formRef = useRef();
+  const inputFocus = useRef();
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClick);
+    return () => { document.removeEventListener("mousedown", handleClick); };
+  }, []);
+
+  const handleClick = e => {
+    if (formRef.current.contains(e.target)) {
+      return;
+    }
+    setBarOpened(false);
+  };
+
+  const onFormSubmit = e => {
+    e.preventDefault();
+    setInput("");
+    setBarOpened(false);
+  };
+
   return (
     <AppContainer>
-      <AddTaskInput/>
+      <TodoListContainer>
+        {(tasks === 0) ? (<TaskTitle>You have no tasks</TaskTitle>) : 
+        ((tasks === 1) ? (<TaskTitle>You have {tasks} task</TaskTitle>) : 
+        (<TaskTitle>You have {tasks} tasks</TaskTitle>))}
+        <InputContainer>
+          <Form
+            barOpened={barOpened}
+            onClick={() => {
+              setBarOpened(true);
+              inputFocus.current.focus();
+            }}
+            onSubmit={onFormSubmit}
+            ref={formRef}
+          >
+            <Button type="submit" barOpened={barOpened}>
+              <AddIcon/>
+            </Button>
+            <Input
+              onChange={e => setInput(e.target.value)}
+              ref={inputFocus}
+              value={input}
+              barOpened={barOpened}
+              placeholder="Add a task"
+            />
+          </Form>
+        </InputContainer>    
+      </TodoListContainer>
     </AppContainer>
   );
 }
--- src/resources/styles/colours.js
+++ src/resources/styles/colours.js
@@ -3,6 +3,7 @@ const colours = {
   secondary: '#fff0bc',
   accent: '#fefcdb',
   complement: '#f7931e',
+  background: '#00204a',
   black: '#000000',
   white: '#ffffff',
 }
